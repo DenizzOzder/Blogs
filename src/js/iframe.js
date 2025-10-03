f4c668bba07c1b05a3bf1c1e3f4a9b3ab1ad6a04
Deniz <candenizozder08@gmail.com>
2025-10-03 15:54:06 +0300

    blog1-review

---
+++ src/js/iframe.js
@@ -0,0 +1,8 @@
+var _coomeet = [];
+                  _coomeet['PartnerId'] = 17098963815615;
+                  _coomeet['appDomain'] = 'me';
+                  (function () {
+                    var coomeet = document.createElement('script'); coomeet.type = 'text/javascript'; coomeet.async = true;
+                    coomeet.src = 'https://iframe.coomeet.me/js/code.js';
+                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(coomeet, s);
+                  })();
